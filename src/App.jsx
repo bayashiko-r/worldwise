b2b917000f002c63744c11e07c7d194bd225a09d
bayashiko-r <mr_chilchilmichil@yahoo.co.jp>
2026-01-03 23:16:03 +0900

    feat: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router";
+import Homepage from "./pages/Homepage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFount";
+
 function App() {
   return (
-    <div>
-      Worldwise
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
