b5e4540378d2ac889919580d077249e5ace8a9e6
bayashiko-r <mr_chilchilmichil@yahoo.co.jp>
2026-01-05 04:48:24 +0900

    feat: use Outlet

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,9 +1,21 @@
 import style from "./AppNav.module.css";
+import { NavLink } from "react-router-dom";
 
 function AppNav() {
     return (
         <nav className={style.nav}>
-            AppNavigation
+            <ul>
+                <li>
+                    <NavLink to="cities">
+                        Cities
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="countries">
+                        Countries
+                    </NavLink>
+                </li>
+            </ul>
         </nav>
     )
 }
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,9 @@
 import styles from "./Map.module.css";
-import Sidebar from "./Sidebar.jsx";
-import Map from "./Map.jsx";
 
 function Map() {
     return (
-        <div className={styles.map}>
-            <Sidebar />
-            <Map />
+        <div className={styles.mapContainer}>
+            Map
         </div>
     )
 }
