f685087ecb6ec42e70e279421e50572e08a986ca
Ryuki Kobayashi <rkobayashi@enthought.com>
2026-01-06 10:32:16 +0900

    feat: use params

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Pricing from "./pages/Pricing";
 import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
+import City from "./components/City";
 import PageNotFound from "./pages/PageNotFound";
 import CountryList from "./components/CountryList";
 
@@ -44,6 +45,7 @@ function App() {
         <Route path="app" element={<AppLayout />}>
           <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
           <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path="cities/:id" element={<City />} />
           <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
