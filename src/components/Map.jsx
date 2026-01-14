66fe0fa475363d4d6c41549f75f207ec7034da31
bayashiko-r <mr_chilchilmichil@yahoo.co.jp>
2026-01-15 00:30:48 +0900

    feat: setting map position with geolocation

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,25 +3,31 @@ import styles from "./Map.module.css";
 import { useSearchParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
+import { useGeolocation } from "../hooks/useGeoLocation";
+import Button from "./Button";
 
 function Map() {
-
     const { cities } = useCities();
     const [mapPosition, setMapPosition] = useState([40, 0]);
-
     const [searchParams, setSearchParams] = useSearchParams();
+    const { isLoading: isLoadingPosition, position: geolocationPosition, getPosition } = useGeolocation();
+
     const mapLat = searchParams.get('lat');
     const mapLng = searchParams.get('lng');
 
     useEffect(function () {
         if (mapLat && mapLng)
             setMapPosition([mapLat, mapLng]);
-    }, [mapLat, mapLng]
+    }, [mapLat, mapLng]);
 
-    );
+    useEffect(function () {
+        if (geolocationPosition)
+            setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+    }, [geolocationPosition])
 
     return (
         <div className={styles.mapContainer}>
+            {!geolocationPosition && <Button type='position' onClick={getPosition}>{isLoadingPosition ? "Loading..." : "Use your position"}</Button>}
             <MapContainer
                 // center={mapPosition}
                 center={[mapLat || 40, mapLng || 0]}
